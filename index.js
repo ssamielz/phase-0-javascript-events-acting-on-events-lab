fcc2f3294d767d8bff434714c87ff16ac993b211
Sam <ssamsanya3@gmail.com>
2025-03-15 08:43:49 +0300

    new

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+const game = document.querySelector("#game");
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    const gameWidth = game.clientWidth; // Width of the game field
+    const dodgerWidth = dodger.clientWidth; // Width of the dodger
+  
+    if (left < 360 ) { // Ensure movement within bounds
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  
+  
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (event.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
